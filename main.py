36c61859256a36b7a7aff7a718d55cb3ccd459b3
valdman40 <valdman40@gmail.com>
2021-03-28 09:11:40 +0300

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    def get(self, name, test):
+        return {"name": name, "test": test}
+
+
+api.add_resource(HelloWorld, "/helloworld/<string: name>/<int: test>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
